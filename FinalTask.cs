9eedb636475e58f82e3fcd98d4f7de298c6a6fb6
YaroslavDubV <yaroslavduboshin@mail.ru>
2022-09-15 00:03:49 +0300

    2 Stage

--- FinalTask.cs
+++ FinalTask.cs
@@ -3,5 +3,9 @@ int n = Convert.ToInt32(Console.ReadLine());
 int m = 0;
 int j = 0;
 string[] array = new string [n];
-
+for (int i = 0; i < n; i++) {
+    Console.WriteLine($"Введите {i + 1} элемент массива");
+    array[i] = Console.ReadLine();
+    if (array[i].Length <= 3) {
+        m += 1;
 
